630fad33378fffb80e64c3ef2541fbf4e86eafc1
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2020-12-29 05:59:28 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,6 @@
 # Write your solution here
 
-current_time = Time.authenticate_or_request_with_http_basic
+current_time = Time.authenticate_or_request_with_http_digest
 current_time = current_time.to_i
 
 if current_time % 2 == 0
